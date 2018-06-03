14545567be58d085d437eff14db02ffb11e6ac8f
Ronel <kroneldo28@yahoo.fr>
2018-06-03 02:37:48 +0200

    Remove the Auth keys from Twilio

--- send_text.py
+++ send_text.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC121feae79893a26b3644cb82bcf74734"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "86614f1cb2a7d7f261476c0d1a6e813c"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+33612495192", 
-    from_="+33757917179",
+    to="", 
+    from_="",
     body="Hello from Python! I sent this message using Twilio on Python")
 
 print(message.sid)
